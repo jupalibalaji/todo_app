02288f91ce4f0afc89e695707ece0b6ee3adf705
jupalibalaji <77019036+jupalibalaji@users.noreply.github.com>
2025-04-16 22:24:56 -0400

    Add Post Api to add tasks

--- src/main/java/com/app/todoapp/controller/TaskController.java
+++ src/main/java/com/app/todoapp/controller/TaskController.java
@@ -5,6 +5,8 @@ import com.app.todoapp.service.TaskService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -21,5 +23,11 @@ public class TaskController {
         tasks.forEach(System.out::println);
         model.addAttribute("tasks", tasks);
         return "tasks";
+    }
+    @PostMapping
+    public String createTask(@RequestParam String title) {
+       taskService.createTask(title);
+       return "redirect:/";
+
     }
 }
